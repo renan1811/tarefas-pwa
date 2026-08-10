2e920310efd0c1dd63f51dcd32f5d66616fa6e95
renan1811 <rennlima477@gmail.com>
2026-08-10 14:59:39 -0300

    FEAT: terminando parte do tutorial 1.7

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
 import './assets/css/global.css';
 
-const app = createApp(App);
+import { registerSW } from 'virtual:pwa-register';
 
+registerSW({
+  immediate: true,
+  onRegisteredSW(swUrl, registration) {
+    if (registration) {
+      setInterval(() => {
+        registration.update();
+      }, 60 * 1000); // verifica a cada 60 segundos
+    }
+  },
+});
+
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+app.use(createPinia());
 app.use(router);
 app.mount('#app');
