29ad8462b960b03d6ded01e90b26924dc77421fa
renan1811 <rennlima477@gmail.com>
2026-08-10 16:22:02 -0300

    FEAT: terminando parte 5.2 do tutorial

--- vite.config.js
+++ vite.config.js
@@ -1,80 +1,19 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { VitePWA } from 'vite-plugin-pwa';
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      registerType: 'prompt',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
-        cleanupOutdatedCaches: true,
-        sourcemap: false,
-        runtimeCaching: [
+      registerType: 'autoUpdate',
+      strategies: 'injectManifest', // 
+      srcDir: 'src',                // 
+      filename: 'sw.js',            // 
 
-
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/._/i, // 
-            handler: 'CacheFirst', // 
-            options: {
-              cacheName: 'google-fonts-cache', // 
-              expiration: {
-                maxEntries: 10, // 
-                maxAgeSeconds: 60 * 60 * 24 * 365, // 
-              },
-              cacheableResponse: {
-                statuses: [0, 200], // 
-              },
-            },
-          },
-
-          {
-            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'gstatic-fonts-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 ano
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-          {
-            urlPattern: /^https:\/\/api\.exemplo\.com\/.*/i,
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'api-cache',
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 60 * 60 * 24, // 24 horas
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-              networkTimeoutSeconds: 10,
-            },
-          },
-          {
-            urlPattern: /^http:\/\/localhost:8001\/.*/i,
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'api-cache',
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 60 * 60 * 24, // 24 horas
-              },
-              networkTimeoutSeconds: 10,
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-        ],
-      },
       manifest: {
         name: 'Gerenciador de Tarefas',
         short_name: 'Tarefas',
@@ -84,6 +23,7 @@ export default defineConfig({
         display: 'standalone',
         scope: '/',
         start_url: '/',
+        id: 'com.task-manager.app',
         icons: [
           {
             src: '/icons/icon-192x192.png',
@@ -103,6 +43,16 @@ export default defineConfig({
           },
         ],
       },
+      devOptions: {
+        enabled: true,
+        type: 'module', // 
+      },
     }),
+    vueDevTools(),
   ],
-});
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
