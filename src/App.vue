d5b630b56707b0ccb24d2766b0ab0465b2a0a7b5
renan1811 <rennlima477@gmail.com>
2026-08-10 14:34:22 -0300

    FEAT: terminando parte do tutorial 1.1

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <AppHeader />
+
+  <main>
+    <router-view />
+  </main>
+</template>
+
+<script setup>
+import AppHeader from './components/AppHeader.vue';
+</script>
+
+<style scoped>
+main {
+  padding-bottom: 40px;
+}
+</style>
